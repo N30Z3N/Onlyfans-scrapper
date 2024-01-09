ecce2379b9e0e2d995e874ed3f4b2041b3255881
Ghost <62809003+Ghost6446@users.noreply.github.com>
2024-01-09 12:23:51 +0100

    fix user data path

--- Src/Util/Helper/driver.py
+++ Src/Util/Helper/driver.py
@@ -8,38 +8,33 @@ from seleniumwire import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 class Driver:
 
-    service = None
-    options = None
-    driver = None
+    def close_chrome_instances(self):
+        if platform.startswith("linux"):
+            try: subprocess.check_output("pkill -f chrome", shell=True)
+            except: pass
+
+        elif platform == "win32":
+            try: subprocess.check_output("TASKKILL /IM chrome.exe /F", shell=True, creationflags=0x08000000)
+            except: pass
 
     def __init__(self) -> None:
-
-        # For linux
-        console.log("[red]Close all instance of chrome")
-        if platform == "linux" or platform == "linux2":
-            try: subprocess.check_output("kill -9 chrome.exe",  shell=True, creationflags=0x08000000) 
-            except: pass
-
-        # For win
-        elif platform == "win32":
-            try: subprocess.check_output("TASKKILL /IM chrome.exe /F",  shell=True, creationflags= 0x08000000) 
-            except: pass
-
+        self.close_chrome_instances()
         self.service = Service(ChromeDriverManager().install())
         self.options = webdriver.ChromeOptions()
 
     def create(self, headless=False, minimize=False):
-
-        if(headless): 
-            self.options.add_argument("headless")
-
+        if headless: self.options.add_argument("headless")
         self.options.add_argument("--window-size=1280,1280")
-        self.options.add_argument('--user-data-dir=C:/Users/'+os.getlogin()+'/AppData/Local/Google/Chrome/User Data')
+
+        user_data_dir = os.path.join(os.path.expanduser('~'), 'AppData', 'Local', 'Google', 'Chrome', 'User Data')
+        if platform == "win32": user_data_dir = user_data_dir.replace('/', '\\')
+        self.options.add_argument(f'--user-data-dir={user_data_dir}')
 
         self.options.add_experimental_option("useAutomationExtension", True)
-        self.options.add_experimental_option("excludeSwitches",["enable-automation"])
+        self.options.add_experimental_option("excludeSwitches", ["enable-automation"])
         self.options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
         self.options.add_argument('--ignore-ssl-errors=yes')
@@ -48,11 +43,11 @@ class Driver:
         self.options.add_argument("--allow-running-insecure-content")
         self.options.add_argument("--disable-notifications")
 
-        self.driver = webdriver.Chrome(options=self.options,  service=self.service)
-    
-        if(minimize):
-            self.driver.minimize_window()
+        self.driver = webdriver.Chrome(options=self.options, service=self.service)
 
+        if minimize:
+            self.driver.minimize_window()
+            
     def get_page(self, url, sleep=1):
         try:
             self.driver.get(url)
