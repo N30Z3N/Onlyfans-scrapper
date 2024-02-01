1d582115dc97f17344a52b10f6e02a6a037b5096
Ghost <62809003+Ghost6446@users.noreply.github.com>
2024-02-01 17:51:58 +0100

    Update ver

--- Src/Util/upload/__version__.py
+++ Src/Util/upload/__version__.py
@@ -1,7 +1,7 @@
 # 31.12.23
 
 __title__ = 'Onlyfans-scraper'
-__version__ = 'v0.9.3'
+__version__ = 'v0.9.4'
 __author__ = 'Ghost6446'
 __description__ = 'A command-line program to download media, posts, and more from OnlyFans'
 __license__ = 'MIT License'
